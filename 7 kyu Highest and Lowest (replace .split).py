e7bd63227e2c66a15fa4e445d4fdaaadad405e95
BabkinDG <denis_babkin@bk.ru>
2024-03-13 07:04:12 +0300

    7 kyu Highest and Lowest Solution replace .split to my algorithm update

--- 7 kyu Highest and Lowest (replace .split).py
+++ 7 kyu Highest and Lowest (replace .split).py
@@ -1,25 +1,21 @@
-numbers = "8 3 -5 42 -1 0 0 -9 4 7 4 -4" # return "42 -9"
+numbers = "8 3 -5 42 -1 0 0 -9 4 7 4 -4"  # return "42 -9"
 
 def high_and_low(numbers):
-
-    def list_split(sym_split, split_string):
-        sym_split_list = []
-        splited_str = ''
+    def list_split(split_sym, split_string):
+        split_list = []
+        split_str = ''
         for i in split_string:
-            if i != sym_split:
-                splited_str += i
-            elif i == sym_split:
-                if splited_str != '':
-                    sym_split_list.append(splited_str)
-                splited_str = ''
-        if splited_str != '':
-            sym_split_list.append(splited_str)
-        return sym_split_list
+            if i != split_sym:
+                split_str += i
+            elif i == split_sym:
+                if split_str != '':
+                    split_list.append(int(split_str))
+                split_str = ''
+        if split_str != '':
+            split_list.append(int(split_str))
+        return split_list
 
-    list = []
-    for i in list_split(' ', numbers):
-        list.append(int(i))
-    print(list)
-    return f'{max(list)} {min(list)}'
+    print(list_split(' ', numbers))
+    return f'{max(list_split(' ', numbers))} {min(list_split(' ', numbers))}'
 
 print(high_and_low(numbers))
