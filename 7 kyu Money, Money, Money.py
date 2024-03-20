7d73232692dee157ab398754d45f003153a8b2e5
BabkinDG <denis_babkin@bk.ru>
2024-03-20 23:12:14 +0300

    add 7 kyu Money, Money, Money solution

--- 7 kyu Money, Money, Money.py
+++ 7 kyu Money, Money, Money.py
@@ -1,7 +1,7 @@
 principal = 1000 # Let P be the Principal = 1000.00
-interest = 0.01625 # Let I be the Interest Rate = 0.01625
+interest = 0.05  # Let I be the Interest Rate = 0.05
 tax = 0.18 # Let T be the Tax Rate = 0.18
-desired = 1200 # Let D be the Desired Sum = 1200
+desired = 1100.00 # Let D be the Desired Sum = 1100.00
 
 
 def calculate_years(principal, interest, tax, desired):
