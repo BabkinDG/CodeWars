77b0addcd9d161df29e0077a2a97571ed9149de4
BabkinDG <denis_babkin@bk.ru>
2024-03-20 23:10:10 +0300

    add 7 kyu Money, Money, Money solution

---
+++ 7 kyu Money, Money, Money.py
@@ -0,0 +1,15 @@
+principal = 1000 # Let P be the Principal = 1000.00
+interest = 0.01625 # Let I be the Interest Rate = 0.01625
+tax = 0.18 # Let T be the Tax Rate = 0.18
+desired = 1200 # Let D be the Desired Sum = 1200
+
+
+def calculate_years(principal, interest, tax, desired):
+    years = 0
+    while principal < desired:
+        years += 1
+        principal += principal * interest - (principal * interest * tax)
+    return years
+    raise NotImplementedError("TODO: calculate_years")
+
+print(calculate_years(principal, interest, tax, desired))
