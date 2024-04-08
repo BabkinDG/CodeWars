e32f058d7e55291b3f6a63a3d12e1641c1e5790d
BabkinDG <denis_babkin@bk.ru>
2024-04-09 02:30:24 +0300

    add 6 kyu Baby Magpies solution

---
+++ 6 kyu Baby Magpies.py
@@ -0,0 +1,30 @@
+bird1 = "BWBWBW"
+bird2 = "BWBWBB"
+bird3 = "WWWWBB"
+
+
+def child(bird1, bird2):
+    difference_list = []
+    for i in range(len(bird1)):
+        difference_list.append(bird1[i] == bird2[i])
+    if bird1 == bird2:
+        return False
+    elif difference_list.count(False) <= 2:
+        return True
+    else:
+        return False
+
+def grandchild(bird1, bird2):
+    difference_list = []
+    for i in range(len(bird1)):
+        difference_list.append(bird1[i] == bird2[i])
+    if bird1 != bird2 and len(bird1) == 1:
+        return False
+    elif difference_list.count(False) <= 4:
+        return True
+    else:
+        return False
+
+
+print(child(bird1, bird2))
+print(grandchild(bird2, bird3))
