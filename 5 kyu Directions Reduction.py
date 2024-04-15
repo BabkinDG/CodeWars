1f18a825d3a9fd7eaab72650fec1bcaedbe23400
BabkinDG <denis_babkin@bk.ru>
2024-04-15 08:06:17 +0300

    add 5 kyu Directions Reduction solution update

--- 5 kyu Directions Reduction.py
+++ 5 kyu Directions Reduction.py
@@ -2,25 +2,23 @@ arr = ["NORTH", "SOUTH", "SOUTH", "EAST", "WEST", "NORTH", "WEST"]  # --> ['WEST
 
 
 def dir_reduc(arr):
-    while ' ' in arr:
-        arr.remove(' ')
     for i in range(len(arr)):
         try:
             if [arr[i], arr[i + 1]] == ["NORTH", "SOUTH"]:
-                arr[i] = ' '
-                arr[i + 1] = ' '
+                arr.pop(i)
+                arr.pop(i)
                 dir_reduc(arr)
             elif [arr[i], arr[i + 1]] == ["SOUTH", "NORTH"]:
-                arr[i] = ' '
-                arr[i + 1] = ' '
+                arr.pop(i)
+                arr.pop(i)
                 dir_reduc(arr)
             elif [arr[i], arr[i + 1]] == ["WEST", "EAST"]:
-                arr[i] = ' '
-                arr[i + 1] = ' '
+                arr.pop(i)
+                arr.pop(i)
                 dir_reduc(arr)
             elif [arr[i], arr[i + 1]] == ["EAST", "WEST"]:
-                arr[i] = ' '
-                arr[i + 1] = ' '
+                arr.pop(i)
+                arr.pop(i)
                 dir_reduc(arr)
         except IndexError:
             continue
