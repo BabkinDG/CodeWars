b83f32854bae1882b11caae270780e459c11aa6d
BabkinDG <denis_babkin@bk.ru>
2024-05-03 02:25:34 +0300

    add 5 kyu Pete, the baker solution

---
+++ 5 kyu Pete, the baker.py
@@ -0,0 +1,15 @@
+recipe = {"flour": 500, "sugar": 200, "eggs": 1}
+available = {"flour": 1200, "sugar": 1200, "eggs": 5, "milk": 200}
+
+
+def cakes(recipe):
+    num_cakes = []
+    try:
+        for k, v in recipe.items():
+            num_cakes.append(available.get(k) // v)
+        return min(num_cakes)
+    except TypeError:
+        return 0
+
+
+print(cakes(recipe))
