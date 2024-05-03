799faf6e264698b515c8d4b7674525e84675e909
BabkinDG <denis_babkin@bk.ru>
2024-05-03 04:17:34 +0300

    add 5 kyu Weight for weight solution update

--- 5 kyu Weight for weight.py
+++ 5 kyu Weight for weight.py
@@ -4,7 +4,6 @@ strng = "2000 10003 1234000 44444444 9999 11 11 22 123"  # --> "11 11 2000 10003
 def order_weight(strng):
     strng_list = strng.split()
     n_list = []
-    strng.split()
     for i in strng_list:
         n_sum = 0
         for n in i:
@@ -15,7 +14,7 @@ def order_weight(strng):
             if n_list[i1] > n_list[i2]:
                 n_list[i1], n_list[i2] = n_list[i2], n_list[i1]
                 strng_list[i1], strng_list[i2] = strng_list[i2], strng_list[i1]
-            elif n_list[i1] == n_list[i2] and str(strng_list[i1]) > str(strng_list[i2]):
+            elif n_list[i1] == n_list[i2] and strng_list[i1] > strng_list[i2]:
                 n_list[i1], n_list[i2] = n_list[i2], n_list[i1]
                 strng_list[i1], strng_list[i2] = strng_list[i2], strng_list[i1]
     return ' '.join(strng_list)
