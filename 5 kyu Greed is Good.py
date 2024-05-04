40bc27b940453c3166eee1ff77d3fd74d5791e78
BabkinDG <denis_babkin@bk.ru>
2024-05-04 23:00:27 +0300

    add 5 kyu Greed is Good solution update

--- 5 kyu Greed is Good.py
+++ 5 kyu Greed is Good.py
@@ -7,18 +7,13 @@ def score(dice):
         points += 1000 + (dice.count(1) - 3) * 100
     if dice.count(1) < 3:
         points += dice.count(1) * 100
-    if dice.count(6) >= 3:
-        points += 600
     if dice.count(5) >= 3:
-        points += 500 + (dice.count(5) - 3) * 50
+        points += (dice.count(5) - 3) * 50
     if dice.count(5) < 3:
         points += dice.count(5) * 50
-    if dice.count(4) >= 3:
-        points += 400
-    if dice.count(3) >= 3:
-        points += 300
-    if dice.count(2) >= 3:
-        points += 200
+    for i in [2, 3, 4, 5, 6]:
+        if dice.count(i) >= 3:
+            points += i * 100
     return points
 
 
