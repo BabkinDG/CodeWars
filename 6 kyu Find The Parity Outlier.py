2f0d289d01e7d4120ef41cdab273df20041151b2
BabkinDG <denis_babkin@bk.ru>
2024-06-28 02:10:45 +0300

    add 6 kyu Find The Parity Outlier solution

---
+++ 6 kyu Find The Parity Outlier.py
@@ -0,0 +1,17 @@
+integers = [2, 4, 0, 100, 4, 11, 2602, 36]  # --> 11
+
+
+def find_outlier(integers):
+    odd, even = [], []
+    for i in integers:
+        if i % 2 == 0:
+            even.append(i)
+        if i % 2 == 1:
+            odd.append(i)
+    if len(even) == 1:
+        return even[0]
+    if len(odd) == 1:
+        return odd[0]
+
+
+print(find_outlier(integers))
